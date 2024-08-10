c7455e710935d27d58c1cf51e13dede9bca3b227
johnitopaisah <johnitopaisah@gmail.com>
2024-08-10 16:03:08 +0200

    setting up the environment

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -14,9 +14,10 @@ class Collection(models.Model):
 
 class Product(models.Model):
     # sku = models.CharField(max_length=10, primary_key=True)
+    slug = models.SlugField(default='-')
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
