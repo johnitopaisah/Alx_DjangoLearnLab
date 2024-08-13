7337abb935e11b5438577e6cf9464761e97ac753
johnitopaisah <johnitopaisah@gmail.com>
2024-08-14 00:39:37 +0200

    retrieving an object

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -4,8 +4,14 @@ from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, Customer, Collection, Order, OrderItem
 
 
+
 def say_hello(request):
-    query_set = OrderItem.objects.filter(product__collection__id=3)
+    product = Product.objects.filter(pk=0).exists()
+    
+    return render(request, 'hello.html', { 'name': 'John Itopa ISAH'})
+
+# def say_hello(request):
+#     query_set = OrderItem.objects.filter(product__collection__id=3)
 
 
-    return render(request, 'hello.html', { 'name': 'John Itopa ISAH', 'products': list(query_set)})
+#     return render(request, 'hello.html', { 'name': 'John Itopa ISAH', 'products': list(query_set)})
