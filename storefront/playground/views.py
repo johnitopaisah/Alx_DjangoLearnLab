2cf263e1aff1d40ba91388c4aac8e0cf04b26cc4
johnitopaisah <johnitopaisah@gmail.com>
2024-08-13 23:56:57 +0200

    setting up the environment

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, Customer, Collection, Order, OrderItem
 
 
 def say_hello(request):
-    query_set = Product.objects.filter(unit_price__range=(20, 30))
+    query_set = OrderItem.objects.filter(product__collection__id=3)
 
 
-    return render(request, 'hello.html', { 'name': 'John', 'products': list(query_set)})
+    return render(request, 'hello.html', { 'name': 'John Itopa ISAH', 'products': list(query_set)})
