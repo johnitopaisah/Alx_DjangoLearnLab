75628aa26f094798e1edc3cc589e265338cee097
johnitopaisah <johnitopaisah@gmail.com>
2024-08-16 23:39:47 +0200

    retrieving an object

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -6,12 +6,10 @@ from store.models import Product, Customer, Collection, Order, OrderItem
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-
-    for product in query_set:
-        print(product)
     
-    return render(request, 'hello.html', { 'name': 'John Itopa ISAH'})
+    query_set = Product.objects.filter(description__isnull=True)
+    
+    return render(request, 'hello.html', { 'name': 'John Itopa ISAH', 'products': list(query_set)})
 
 # def say_hello(request):
 #     query_set = OrderItem.objects.filter(product__collection__id=3)
