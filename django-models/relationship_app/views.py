a134d1c05cdfef58a875a17dc7a53e95e08a3540
johnitopaisah <johnitopaisah@gmail.com>
2024-08-18 21:59:18 +0200

    Add function-based and class-based views, and configure URL patterns

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,9 +3,13 @@ from django.views.generic import DetailView
 from .models import Library
 from .models import Book
 
+from django.shortcuts import render
+from .models import Book
+
+
 def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()  # Fetch all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 class LibraryDetailView(DetailView):
