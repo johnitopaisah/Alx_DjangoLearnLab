0839dbedaf9d2ebd444c9caf016a28c77d33ffe0
johnitopaisah <johnitopaisah@gmail.com>
2024-08-18 21:24:01 +0200

    setting up the task 0

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -4,7 +4,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 def run_queries():
     # Query all books by a specific author
     author_name = "Author Name"  # replace with the actual author's name
-    books_by_author = Book.objects.filter(author__name=author_name)
+    author = Author.objects.get(name=author_name)  # Use get() to fetch the specific author
+    books_by_author = Book.objects.filter(author=author)  # Use filter() to get all books by that author
     print(f"Books by {author_name}:")
     for book in books_by_author:
         print(book.title)
