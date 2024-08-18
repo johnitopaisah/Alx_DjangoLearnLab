1a049695f5d6baaf300875211832892fc3967093
johnitopaisah <johnitopaisah@gmail.com>
2024-08-18 22:23:01 +0200

    Add function-based and class-based views, and configure URL patterns

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 
+from django.urls import path
+from .views import 
+
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', login_view, name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LogoutView
+
 from .models import Library
 from .models import Book
 
@@ -17,3 +26,26 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+# Registration View
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')
+
+# Login View
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your desired redirect URL
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+# Logout View
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
