3d8444c61f3b4f46ad3903172f81972f59a87c3c
johnitopaisah <johnitopaisah@gmail.com>
2024-08-18 22:01:42 +0200

    Add function-based and class-based views, and configure URL patterns

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
+
 
 from django.shortcuts import render
 from .models import Book
