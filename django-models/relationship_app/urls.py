064a875b1c35d8eac72c0cce11ceef55a9327182
johnitopaisah <johnitopaisah@gmail.com>
2024-08-18 22:38:21 +0200

    Add function-based and class-based views, and configure URL patterns

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from .views import login_view, CustomLogoutView, RegisterView
 
-from django.urls import path
-from .views import 
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', login_view, name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+
+    # Authentication URLs
+    path('login/', login_view, name='login'),  # Function-based view for login
+    path('logout/', CustomLogoutView.as_view(), name='logout'),  # Class-based view for logout
+    path('register/', RegisterView.as_view(), name='register'),  # Class-based view for registration
 ]
