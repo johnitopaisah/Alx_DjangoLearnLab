6783e56f1e140400f66e89a1b6303cc9311c8d19
JohnItopaIsah <johnitopaisah@gmail.com>
2024-09-24 08:21:23 +0200

    updated the views.py file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_list_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.models import Group
+from .forms import ExampleForm
 
 
 def register(request):
